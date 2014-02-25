6fd739d8fb7eeddfc6261cea8e3362438ab389aa
pullis <mika.lehtimaki1@gmail.com>
2014-02-25 15:28:52 +0200

    test

--- Home configs/.zprofile
+++
@@ -1,3 +0,0 @@
-export XDG_CONFIG_HOME="/home/rolle/.config"
-export BSPWM_SOCKET="/tmp/bspwm-socket"
-export PANEL_FIFO="/tmp/panel-fifo"
--- Home configs/wp/luo_teemat.sh
+++
@@ -1,5 +0,0 @@
-for file in /home/rolle/wp/TAUSTAT/*
-do
-  echo "Adding $file to colorscheme supported wp's.."
-  ~/wp/wp "add $file" 
-done
